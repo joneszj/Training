958f9a7645197d2579f00e5447f70e7ec10a4349
joneszj <joneszj@gmail.com>
2022-05-05 23:00:24 -0400

    updates

---
+++ GatheringGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GatheringGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We are tasked with creating a gathering mini-game. Using classes we will create 3 forms of gathering: mining, farming, and fishing. (like a popular MMO)
+            // The player charcter (user) will first select what form of gathering they would like to do
+            // The user must then select the item they will search for. Each item is specific to a form of gathering
+            // So a stone is only available to mining, a corn is only available to farming, and a bass is only available to fishing
+            // Make at least 5 items per gathering form to select from
+            // Using an interface for gathering, allow the user to gather 1 item per turn, with a chance of failing (null)
+            // Allow five attempts, quit gathering on a successful gather, or after 5 unseccessful attempt
+            // Because we are using an interface, we will not use any control flow construct when starting the gathering
+            // Ex: no 'if miner start mining'
+            // Keep track of users inventory and output it when the mini-game is finished (user indicates they wish to quit gathering)
+        }
+    }
+}
--- Members/Program.cs
+++ Members/Program.cs
@@ -8,22 +8,53 @@ namespace Members
         {
             // https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/program-building-blocks
             // A member is a general term refering to any thing that is on a class
-            
+
+            #region Static vs Instance
             // Members may be static or instanced
             // static members are called on the class, and any static state is shared among all instance of the class
             // non-static members are on instances of the class, and their state is isolated to the instance
-            // non-static members have access to static members, but not the other way around 
-            
+            // non-static members have access to static members, but not the other way around  
+            #endregion
+
+            #region Common Members
+            // fields - state on a class
+                // fields may be marked 'readonly' meaning the value can only be set at declaration or in a constructor
+            // properties - state on a class, is a getter setter wrapper of a field
+                // auto-properties (most common form, also called properties) - state on a class, is a getter setter wrapper of a field, and the field is automatically hidden
+                // properties can be made readonly, or write only by ommiting the setter or the getter respectively
+                // properties are an example/implementation of the encapsulation OOP principle: https://docs.microsoft.com/en-us/dotnet/csharp/fundamentals/tutorials/oop
+            // methods - execute code blocks
+            // constructors - execute when a class object is instantiated, and for parameters to be supplied that are required for the object to function properly
+            #endregion
+
+            #region Accessibility
             // Members have accessibility values controlling where they can be accessed: https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/program-building-blocks#accessibility
-            // By default, without any access modifier, members are internal (only visible to the assembly)
+            // By default, without any accessibility modifier, members are internal (only visible to the other entities/classes in the assembly)
             // Most access modifiers in my experience have been using public or private
-            // public means there is no restriction, private means the member is only accessible to the class they are defined on
+            // public means there is no restriction, private means the member is only accessible to the class they are defined on 
+            #endregion
         }
 
         class MyClass
         {
+            // A default constructor (ctor + tab + tab)
+            public MyClass()
+            {
+
+            }
+
+            // A constructor with a required paremeter
+            public MyClass(int fieldNumber)
+            {
+                // set this instances myField
+                myReadOnlyField = fieldNumber;
+            }
+
             // A field
             int myField;
+
+            // A readonly field, value can only be set at declaration or in a constructor
+            readonly int myReadOnlyField;
 
             // A full-property (or just property)
             private int myProperty;
