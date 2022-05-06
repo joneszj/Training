1f9095e80262be45174dfd0c81a2351230a4128a
joneszj <joneszj@gmail.com>
2022-05-06 01:45:06 -0400

    updates

---
+++ HardZoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HardZoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SmartZoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartZoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
