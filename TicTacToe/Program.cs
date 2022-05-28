48c348e4b358ccfef42af964b89cd710f0f8c3a2
joneszj <joneszj@gmail.com>
2022-05-28 16:53:10 -0400

    updates

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,20 +1,352 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// this works, but with the basics, we can see how things can get way out of hand and difficult to read
+
+var r1c1 = "*";
+var r1c2 = "*";
+var r1c3 = "*";
+var r2c1 = "*";
+var r2c2 = "*";
+var r2c3 = "*";
+var r3c1 = "*";
+var r3c2 = "*";
+var r3c3 = "*";
+string winner = string.Empty;
 
 // display board
+Console.WriteLine($" {r1c1} | {r1c2} | {r1c3} ");
+Console.WriteLine($" {r2c1} | {r2c2} | {r2c3} ");
+Console.WriteLine($" {r3c1} | {r3c2} | {r3c3} ");
 
-// ask for player X turn
-// validate move
+do
+{
+    // ask for player X turn
+    Console.WriteLine("Player X, please enter your move: ");
+    Console.WriteLine("Row: ");
+    var row = int.Parse(Console.ReadKey().KeyChar.ToString());
+    Console.WriteLine("Column: ");
+    var column = int.Parse(Console.ReadKey().KeyChar.ToString());
 
-// render move on board
+    if (row == 1)
+    {
+        if (column == 1)
+        {
+            if (r1c1 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r1c1 = "X";
+            }
+        }
+        else if (column == 2)
+        {
+            if (r1c2 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r1c2 = "X";
+            }
+        }
+        else
+        {
+            if (r1c3 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r1c3 = "X";
+            }
+        }
+    }
+    else if (row == 2)
+    {
+        if (column == 1)
+        {
+            if (r2c1 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r2c1 = "X";
+            }
+        }
+        else if (column == 2)
+        {
+            if (r2c2 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r2c2 = "X";
+            }
+        }
+        else
+        {
+            if (r2c3 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r2c3 = "X";
+            }
+        }
+    }
+    else
+    {
+        if (column == 1)
+        {
+            if (r3c1 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r3c1 = "X";
+            }
+        }
+        else if (column == 2)
+        {
+            if (r3c2 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r3c2 = "X";
+            }
+        }
+        else
+        {
+            if (r3c3 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r3c3 = "X";
+            }
+        }
+    }
 
-// check for win - a
+    // display board
+    Console.Clear();
+    Console.WriteLine($" {r1c1} | {r1c2} | {r1c3} ");
+    Console.WriteLine($" {r2c1} | {r2c2} | {r2c3} ");
+    Console.WriteLine($" {r3c1} | {r3c2} | {r3c3} ");
 
-// ask for player O turn
-// validate move
+    // check for win - a
+    if (r1c1 == r1c2 && r1c2 == r1c3 && r1c1 != "*")
+    {
+        winner = "X";
+    }
+    else if (r2c1 == r2c2 && r2c2 == r2c3 && r2c1 != "*")
+    {
+        winner = "X";
+    }
+    else if (r3c1 == r3c2 && r3c2 == r3c3 && r3c1 != "*")
+    {
+        winner = "X";
+    }
+    else if (r1c1 == r2c1 && r2c1 == r3c1 && r1c1 != "*")
+    {
+        winner = "X";
+    }
+    else if (r1c2 == r2c2 && r2c2 == r3c2 && r1c2 != "*")
+    {
+        winner = "X";
+    }
+    else if (r1c3 == r2c3 && r2c3 == r3c3 && r1c3 != "*")
+    {
+        winner = "X";
+    }
+    else if (r1c1 == r2c2 && r2c2 == r3c3 && r1c1 != "*")
+    {
+        winner = "X";
+    }
+    else if (r1c3 == r2c2 && r2c2 == r3c1 && r1c3 != "*")
+    {
+        winner = "X";
+    }
+    else if (r1c1 != "*" && r1c2 != "*" && r1c3 != "*" && r2c1 != "*" && r2c2 != "*" && r2c3 != "*" && r3c1 != "*" && r3c2 != "*" && r3c3 != "*")
+    {
+        winner = "No player";
+    }
+    
+    if (!string.IsNullOrEmpty(winner))
+    {
+        break;
+    }
 
-// render move on board
+    // ask for player O turn
+    Console.WriteLine("Player O, please enter your move: ");
+    Console.WriteLine("Row: ");
+    row = int.Parse(Console.ReadKey().KeyChar.ToString());
+    Console.WriteLine("Column: ");
+    column = int.Parse(Console.ReadKey().KeyChar.ToString());
+    if (row == 1)
+    {
+        if (column == 1)
+        {
+            if (r1c1 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r1c1 = "O";
+            }
+        }
+        else if (column == 2)
+        {
+            if (r1c2 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r1c2 = "O";
+            }
+        }
+        else
+        {
+            if (r1c3 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r1c3 = "O";
+            }
+        }
+    }
+    else if (row == 2)
+    {
+        if (column == 1)
+        {
+            if (r2c1 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r2c1 = "O";
+            }
+        }
+        else if (column == 2)
+        {
+            if (r2c2 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r2c2 = "O";
+            }
+        }
+        else
+        {
+            if (r2c3 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r2c3 = "O";
+            }
+        }
+    }
+    else
+    {
+        if (column == 1)
+        {
+            if (r3c1 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r3c1 = "O";
+            }
+        }
+        else if (column == 2)
+        {
+            if (r3c2 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r3c2 = "O";
+            }
+        }
+        else
+        {
+            if (r3c3 != "*")
+            {
+                Console.Beep();
+            }
+            else
+            {
+                r3c3 = "O";
+            }
+        }
+    }
 
-// check for win - a
+    // display board
+    Console.Clear();
+    Console.WriteLine($" {r1c1} | {r1c2} | {r1c3} ");
+    Console.WriteLine($" {r2c1} | {r2c2} | {r2c3} ");
+    Console.WriteLine($" {r3c1} | {r3c2} | {r3c3} ");
 
-// a. if win found, exit game, declare winner
+    if (r1c1 == r1c2 && r1c2 == r1c3 && r1c1 != "*")
+    {
+        winner = "O";
+    }
+    else if (r2c1 == r2c2 && r2c2 == r2c3 && r2c1 != "*")
+    {
+        winner = "O";
+    }
+    else if (r3c1 == r3c2 && r3c2 == r3c3 && r3c1 != "*")
+    {
+        winner = "O";
+    }
+    else if (r1c1 == r2c1 && r2c1 == r3c1 && r1c1 != "*")
+    {
+        winner = "O";
+    }
+    else if (r1c2 == r2c2 && r2c2 == r3c2 && r1c2 != "*")
+    {
+        winner = "O";
+    }
+    else if (r1c3 == r2c3 && r2c3 == r3c3 && r1c3 != "*")
+    {
+        winner = "O";
+    }
+    else if (r1c1 == r2c2 && r2c2 == r3c3 && r1c1 != "*")
+    {
+        winner = "O";
+    }
+    else if (r1c3 == r2c2 && r2c2 == r3c1 && r1c3 != "*")
+    {
+        winner = "O";
+    }
+    else if (r1c1 != "*" && r1c2 != "*" && r1c3 != "*" && r2c1 != "*" && r2c2 != "*" && r2c3 != "*" && r3c1 != "*" && r3c2 != "*" && r3c3 != "*")
+    {
+        winner = "No player";
+    }
+
+    // display board
+    Console.Clear();
+    Console.WriteLine($" {r1c1} | {r1c2} | {r1c3} ");
+    Console.WriteLine($" {r2c1} | {r2c2} | {r2c3} ");
+    Console.WriteLine($" {r3c1} | {r3c2} | {r3c3} ");
+
+} while (string.IsNullOrEmpty(winner));
+
+Console.WriteLine($"{winner} wins!");
---
+++ TicTakToe2/Program.cs
@@ -0,0 +1,89 @@
+﻿// this is better than the previous one, but there is still room for improvement
+
+// initialize game
+var game = new char[3, 3];
+var players = new char[] { 'X', 'O' };
+var winner = new char();
+int row, column;
+for (int i = 0; i < 3; i++)
+{
+    for (int y = 0; y < 3; y++)
+    {
+        game[i, y] = '*';
+    }
+}
+
+// display board
+for (int i = 0; i < 3; i++)
+{
+    for (int y = 0; y < 3; y++)
+    {
+        Console.Write(" | " + game[i, y] + " | ");
+    }
+    Console.WriteLine();
+}
+
+do
+{
+    for (int z = 0; z < players.Length; z++)
+    {
+        var invalid = true;
+        do
+        {
+            // player turn
+            Console.WriteLine("Player " + players[z] + "'s turn.");
+            Console.Write("Enter row: ");
+            if (!int.TryParse(Console.ReadLine()!, out row))
+            {
+                Console.WriteLine("Invalid input. Try again.");
+                continue;
+            }
+            row--;
+            Console.Write("Enter column: ");
+            if (!int.TryParse(Console.ReadLine()!, out column))
+            {
+                Console.WriteLine("Invalid input. Try again.");
+                continue;
+            }
+            column--;
+            if (game[row, column] == '*')
+            {
+                game[row, column] = players[z];
+                invalid = false;
+                // check for winner
+                if (game[0, 0] == game[0, 1] && game[0, 1] == game[0, 2] && game[0, 0] != '*'
+                    || game[1, 0] == game[1, 1] && game[1, 1] == game[1, 2] && game[1, 0] != '*'
+                    || game[2, 0] == game[2, 1] && game[2, 1] == game[2, 2] && game[2, 0] != '*'
+                    || game[0, 0] == game[1, 0] && game[1, 0] == game[2, 0] && game[0, 0] != '*'
+                    || game[0, 1] == game[1, 1] && game[1, 1] == game[2, 1] && game[0, 1] != '*'
+                    || game[0, 2] == game[1, 2] && game[1, 2] == game[2, 2] && game[0, 2] != '*'
+                    || game[0, 0] == game[1, 1] && game[1, 1] == game[2, 2] && game[0, 0] != '*'
+                    || game[0, 2] == game[1, 1] && game[1, 1] == game[2, 0] && game[0, 2] != '*')
+                {
+                    winner = players[z];
+                }
+            }
+            else
+            {
+                Console.WriteLine("Position taken. Try again...");
+            }
+        } while (invalid);
+
+        // display board
+        Console.Clear();
+        for (int i = 0; i < 3; i++)
+        {
+            for (int y = 0; y < 3; y++)
+            {
+                Console.Write(" | " + game[i, y] + " | ");
+            }
+            Console.WriteLine();
+        }
+
+        if (winner != default)
+        {
+            break;
+        }
+    }
+} while (winner == default);
+Console.WriteLine("Player " + winner + " wins!");
